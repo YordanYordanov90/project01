77c31ca945aae390062135d789c73d56085a2ab2
YordanYordanov90 <157980191+YordanYordanov90@users.noreply.github.com>
2024-04-21 11:13:54 +0300

    last

--- src/app/sections/Navbar.tsx
+++ src/app/sections/Navbar.tsx
@@ -8,16 +8,16 @@ const Navbar = () =>{
               Website
             </h3>
             <div className='text-custom-text space-x-8 ml-16 items-center text-xl hidden md:flex'>
-              <a href="" className='hover:text-red-500 '>Начало</a>
-              <a href="" className='hover:text-red-500'>За нас</a>
-              <a href="" className='hover:text-red-500'>Услуги</a>
-              <a href="" className='hover:text-red-500'>Цени</a>
-              <a href="" className='hover:text-red-500'>Как работи</a>
-              <a href="" className='hover:text-red-500'>Контакти</a>
+              <a href="" className=' transition-transform duration-300 hover:scale-110 hover:text-red-500 '>Начало</a>
+              <a href="" className='transition-transform duration-300 hover:scale-110 hover:text-red-500'>За нас</a>
+              <a href="" className='transition-transform duration-300 hover:scale-110 hover:text-red-500'>Услуги</a>
+              <a href="" className='transition-transform duration-300 hover:scale-110 hover:text-red-500'>Цени</a>
+              <a href="" className='transition-transform duration-300 hover:scale-110 hover:text-red-500'>Как работи</a>
+              <a href="" className='transition-transform duration-300 hover:scale-110 hover:text-red-500'>Контакти</a>
             </div>
             <div className='text-custom-text space-x-4 p-4 text-xl items-center hidden md:flex'>
-              <a href="" className='hover:text-red-500'>Профил</a>
-              <button className='hover:text-red-500 border-2 border-black p-3 m;-1 rounded-lg'>Заявка за пратка </button>
+              <a href="" className='transition-transform duration-300 hover:scale-110 hover:text-red-500'>Профил</a>
+              <button className='transition-transform duration-300 hover:scale-110 hover:text-red-500 border-2 border-black p-3 m;-1 rounded-lg'>Заявка за пратка </button>
             </div>
           </div>
           
